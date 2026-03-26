28bdfd43d61695c0c29053401e2c912c0f7db83b
Hamdallah <hamdaraksa@gmail.com>
2026-03-27 00:29:31 +0700

    fix form

--- main.js
+++ main.js
@@ -42,34 +42,34 @@ faqs.forEach((faq) => {
 });
 
 const statusMsg = document.getElementById("status-msg");
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzC1ALxwxtLDOhNxdzt-IFg0Bz_we-g9Qf6ETi6_jMH_tvKVVE52OGDkI5cnZkPH8ui/exec'
+const scriptURL = 'https://script.google.com/macros/s/AKfycbz-uDSlszWlWgJBU0PqV5qWueqEPzJTTiHy5USOrNfzCtCN3eXLf6FUaLkx2uJ6M27F/exec'
 const form = document.forms['Contact-form'];
 const buttonSubmit = document.getElementById("button-submit");
 
 function validateUserName(username) {
     const userNameRegex = /^[a-zA-Z0-9]{3,}$/;
     return userNameRegex.test(username);
-} 
+}
 function validateEmail(email) {
     const emailRegex = /^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/;
     return emailRegex.test(email);
-} 
+}
 
 function validateForm() {
     let username = document.getElementById("nama").value.trim();
     let email = document.getElementById("email").value.trim();
-    if(!validateUserName(username)){
+    if (!validateUserName(username)) {
         statusMsg.style.display = "block";
-        statusMsg.textContent = "username tidak valid";
+        statusMsg.textContent = "username not valid";
         statusMsg.style.color = "red";
         setTimeout(() => {
             statusMsg.style.display = "none";
         }, 5000)
         return false;
     }
-    if(!validateEmail(email)){        
+    if (!validateEmail(email)) {
         statusMsg.style.display = "block";
-        statusMsg.textContent = "email tidak valid";
+        statusMsg.textContent = "email not valid";
         statusMsg.style.color = "red";
         setTimeout(() => {
             statusMsg.style.display = "none";
@@ -83,29 +83,32 @@ form.addEventListener('submit', e => {
     e.preventDefault();
     if (!validateForm()) return;
 
-statusMsg.style.display = "block";
-statusMsg.textContent = "Sedang dikirim...";
-statusMsg.style.color = "#00ccff";
-buttonSubmit.disabled = true;
+    statusMsg.style.display = "block";
+    statusMsg.textContent = "Sedang dikirim...";
+    statusMsg.style.color = "#00ccff";
+    buttonSubmit.disabled = true;
 
 
-fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => {
-        statusMsg.textContent = "Pesan berhasil dikirim";
-        statusMsg.style.color = "green";
-        console.log('Success!', response);
-        form.reset();
+    fetch(scriptURL, {
+        method: 'POST',
+        mode: 'no-cors',
+        body: new FormData(form)
     })
-    .catch(error => {
-        statusMsg.textContent = "Pesan gagal dikirim";
-        statusMsg.style.color = "red";
-        console.error('Error!', error.message);
-    })
-    .finally(() => {
-        buttonSubmit.disabled = false;
-        setTimeout(() => {
-            statusMsg.style.display = "none";
+        .then(() => {
+            statusMsg.textContent = "message success to send";
+            statusMsg.style.color = "green";
             form.reset();
-        }, 5000);
-    })
+        })
+        .catch(error => {
+            statusMsg.textContent = "message fail to send";
+            statusMsg.style.color = "red";
+            console.error('Error!', error.message);
+        })
+        .finally(() => {
+            buttonSubmit.disabled = false;
+            setTimeout(() => {
+                statusMsg.style.display = "none";
+                form.reset();
+            }, 5000);
+        })
 })
